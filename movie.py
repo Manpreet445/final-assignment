8c4a4a0228c154ee7db7708b2bfe353e63fe75b9
Manpreet Singh <ManpreetSingh3@edu.sait.ca>
2025-04-16 22:42:45 -0600

    added a class file

---
+++ movie.py
@@ -0,0 +1,87 @@
+class Movie:
+    GENRE_NAMES = [
+        "Action", "Comedy", "Drama", "Horror", "Sci-Fi", 
+        "Romance", "Thriller", "Animation", "Documentary", "Fantasy"
+    ]
+    
+    def __init__(self, movie_id, title, director, genre, available=True, price=0.0, fine_rate=0.0, rental_count=0):
+        self.__id = movie_id
+        self.__title = title
+        self.__director = director
+        self.__genre = genre
+        self.__available = available
+        self.__price = price
+        self.__fine_rate = fine_rate
+        self.__rental_count = rental_count
+
+    # Getters
+    def get_id(self):
+        return self.__id
+
+    def get_title(self):
+        return self.__title
+
+    def get_director(self):
+        return self.__director
+
+    def get_genre(self):
+        return self.__genre
+
+    def get_price(self):
+        return self.__price
+
+    def get_fine_rate(self):
+        return self.__fine_rate
+
+    def get_rental_count(self):
+        return self.__rental_count
+
+    def get_genre_name(self):
+        return Movie.GENRE_NAMES[self.__genre]
+
+    def get_availability(self):
+        return "Available" if self.__available else "Rented"
+
+    # Setters
+    def set_id(self, movie_id):
+        self.__id = movie_id
+
+    def set_title(self, title):
+        self.__title = title
+
+    def set_director(self, director):
+        self.__director = director
+
+    def set_genre(self, genre):
+        self.__genre = genre
+
+    def set_price(self, price):
+        self.__price = price
+
+    def set_fine_rate(self, fine_rate):
+        self.__fine_rate = fine_rate
+
+    # Borrow movie
+    def borrow_movie(self):
+        if self.__available:
+            self.__available = False
+            self.__rental_count += 1
+        else:
+            print(f"The movie {self.__title} is already rented.")
+
+    # Return movie
+    def return_movie(self):
+        self.__available = True
+
+    # String representation
+    def __str__(self):
+        return "{:^10}{:^30}{:^25}{:^15}{:^15}{:^15.2f}{:^15.2f}{:^15}".format(
+            str(self.__id),
+            self.__title,
+            self.__director,
+            self.get_genre_name(),
+            self.get_availability(),
+            self.__price,
+            self.__fine_rate,
+            str(self.__rental_count)
+        )
